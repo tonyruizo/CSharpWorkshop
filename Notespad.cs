938be0ce7243224eb8409190fac8e1a925357d5c
Tonyruizo <tonyruizo@pm.me>
2023-08-02 13:17:03 -0400

    File to experiment/test code

---
+++ Notespad.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpWorkshop
+{
+    internal class Notespad
+    {
+        public static void Run()
+        {
+            Console.WriteLine();
+            Console.WriteLine("I test random code in this file. Will stay empty.");
+            Console.WriteLine();
+        }
+
+
+    }
+}
