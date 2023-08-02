168147b17399e979fa58857400526231818c8d8d
Tonyruizo <tonyruizo@pm.me>
2023-08-02 13:16:21 -0400

    Additional conditions added. File names are displayed if none is entered.

--- Program.cs
+++ Program.cs
@@ -1,18 +1,29 @@
-﻿namespace CSharpWorkshop
+﻿using Microsoft.VisualBasic;
+using System.Reflection;
+using System.Threading;
+
+namespace CSharpWorkshop
 {
     public class Program
     {
+        // My entry point "Main()" to my console application
         static void Main(string[] args)
         {
+            // Check if any command-line argument is provided
             if (args.Length > 0)
             {
+                // Convert the argument to lowercase to ensure comparison
                 switch (args[0].ToLower())
                 {
+                    // if "calculator is matched, will execute Run() method in Calculator Class
                     case "calculator":
                         Calculator.Run();
                         break;
                     case "todo":
                         Todo.Run();
+                        break;
+                    case "notespad": // I test random code in this file.
+                        Notespad.Run();
                         break;
                     default: Console.WriteLine("Invalid Exercise file, please check the name and try again.");
                         break;
@@ -21,8 +32,52 @@
             else
             {
                 Console.WriteLine();
-                Console.WriteLine("Please enter the exercise file name you wish to run. (Example) dotnet run 'filename' (do not use quotes) then press enter.");
+                Console.WriteLine("Please enter the exercise file name you wish to run. Type: dotnet run 'filename' (do not use quotes) then press enter.");
+                Console.WriteLine();
+                // Delay the next message 3.5 seconds
+                Thread.Sleep(3500);
+
+                // Calls method and assigns the returned array of strings to the variable "fileNames"
+                string[] fileNames = GetAvailableCases();
+
+                Console.WriteLine("The available files to run are:");
+                Console.WriteLine();
+
+                // Loop through fileNames array and display each index
+                foreach (string file in fileNames)
+                {
+                    Console.WriteLine(file);
+                }
             }
+        }
+
+        // GetAvailableCases Method that returns an array of strings
+        static string[] GetAvailableCases()
+        {
+            // Use Reflection to inspect the methods in other classes
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type[] types = assembly.GetTypes();
+
+            // Define a list to store the available case names
+            System.Collections.Generic.List<string> caseNames = new System.Collections.Generic.List<string>();
+
+            foreach (Type type in types)
+            {
+                // Check if the type is a class and not the Program class itself
+                if (type.IsClass && type.Name != "Program")
+                {
+                    // Check if the class has a method named "Run"
+                    MethodInfo? methodInfo = type.GetMethod("Run", BindingFlags.Static | BindingFlags.Public);
+                    if (methodInfo != null)
+                    {
+                        // Add the class name (case name) to the list of available cases
+                        caseNames.Add(type.Name.ToLower());
+                    }
+                }
+            }
+
+            // Convert the list to an array and return it
+            return caseNames.ToArray();
         }
     }
 }
