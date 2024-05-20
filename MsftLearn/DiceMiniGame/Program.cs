0feaeae3df4e2cdb48010286de4fd9406cc213fb
Tonyruizo <tonyruizo@pm.me>
2024-05-20 09:45:08 -0400

    Challenges

---
+++ MsftLearn/DiceMiniGame/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Xml.XPath;
+
+namespace DiceMiniGame;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Select a target from random roll between 1-5.
+        // user must roll six-diced dice.
+        // To win, the player must roll a number > target
+        // Ask player to play again.
+
+        Random roll = new Random();
+        bool playAgain = true;
+
+
+        Console.WriteLine("Welcome to Dice Game! Let's get started!");
+
+        while (playAgain)
+        {
+            Console.WriteLine("\nLets roll the target..");
+            int targetNumber = RollDice();
+
+            Console.WriteLine($"Target number: {targetNumber}");
+
+            System.Console.WriteLine("\nLets roll your dice...");
+            int userRoll = RollDice();
+
+            System.Console.WriteLine($"Your Roll: {userRoll}");
+
+            // Check win or loose
+            Console.WriteLine(WinOrLose(targetNumber, userRoll));
+
+            // Ask to play again
+            playAgain = ShouldPlay();
+
+        }
+
+        // Method to determine if win or loose
+        string WinOrLose(int target, int userRoll)
+        {
+            return (userRoll > target) ? "You won!" : "You loose! :(";
+        }
+
+        // Method to determine to play again or not.
+        bool ShouldPlay()
+        {
+            Console.WriteLine("Would you like to play again? (Y/N)");
+            string userAnswer = Console.ReadLine()!;
+
+            if (userAnswer == "Y".ToLower())
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        // Method to get random roll
+        int RollDice()
+        {
+            return roll.Next(1, 7);
+        }
+
+    }
+}
---
+++ MsftLearn/IntegralTypes/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace IntegralTypes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        System.Console.WriteLine("Data Types stored in the Stack");
+        System.Console.WriteLine("");
+        Console.WriteLine("Signed inegral types: ");
+        System.Console.WriteLine($"sbyte: {sbyte.MinValue} to {sbyte.MaxValue}");
+        System.Console.WriteLine($"short: {short.MinValue} to {short.MaxValue}");
+        System.Console.WriteLine($"int: {int.MinValue} to {int.MaxValue}");
+        System.Console.WriteLine($"long: {long.MinValue} to {long.MaxValue}");
+
+        Console.WriteLine("");
+        Console.WriteLine("Unsigned integral types:");
+
+        Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+        Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+        Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+        Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+
+        Console.WriteLine("");
+        Console.WriteLine("Floating point types:");
+        Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+        Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+        Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+
+        System.Console.WriteLine("");
+        System.Console.WriteLine("Data Types stored in the Heap");
+        System.Console.WriteLine("");
+
+        int[] data = new int[3];
+    }
+}
